310ff6696ccdb1579f33082d87af02a6f072a800
jeil <sjl01002@gmail.com>
2024-09-03 13:33:24 +0900

    feat:login

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('', views.SignupAPIView.as_view())
+    path('', views.SignupAPIView.as_view()),
+    path('login/', views.CustomTokenObtainPairView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .serializers import SignupSerializer
 
 class SignupAPIView(APIView):
@@ -9,3 +11,13 @@ class SignupAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    default_error_messages = {
+        "no_active_account": ("아이디/비밀번호를 다시확인해주세요.")
+    }
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
