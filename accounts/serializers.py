435df0e23e23539f47c8ca44916e7195f6d544d9
jeil <sjl01002@gmail.com>
2024-09-03 16:51:43 +0900

    feat:check login

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = '__all__'
+
+
+class ProfileSerializer(SignupSerializer):
+    pass
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from accounts import views
 
+
 urlpatterns = [
     path('', views.SignupAPIView.as_view()),
     path('login/', views.CustomTokenObtainPairView.as_view()),
+    path('<str:username>/', views.ProfileAPIView.as_view()),
 ]
