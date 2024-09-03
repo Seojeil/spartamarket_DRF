011cc14b97c36e6462c2e450f00e5ab0e0e6e9dc
jeil <sjl01002@gmail.com>
2024-09-03 21:06:05 +0900

    fix:signup

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
 
 class SignupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
-        fields = '__all__'
+        model = User
+        fields = ('email', 'username', 'password', 'nickname', 'birth_day', 'first_name', 'last_name')
+        extra_kwargs = {'password': {'write_only': True}}
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 
 class ProfileSerializer(SignupSerializer):
--- products/views.py
+++ products/views.py
@@ -34,15 +34,28 @@ class ProductDetailAPIView(APIView):
         return Response(serializer.data)
 
     def put(self, request, pk):
-        product = Product.objects.get(pk=pk)
-        if product.pk == request.user:
-            serializer = ProductDetailSerializer(product, data=request.data, partial=True)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data)
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({"존재하지 않는 글입니다."}, status=status.HTTP_404_NOT_FOUND)
+        
+        if product.author != request.user:
+            return Response({"작성자가 일치하지 않습니다."}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = ProductDetailSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
         
         
     def delete(self, request, pk):
-        product = Product.objects.get(pk=pk)
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({"존재하지 않는 글입니다."}, status=status.HTTP_404_NOT_FOUND)
+        
+        if product.author != request.user:
+            return Response({"작성자가 일치하지 않습니다."}, status=status.HTTP_403_FORBIDDEN)
+        
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
